4704bd63631dda8f4913c5f6a7e246f50f3b5445
hafizahhassan <hfzhhssn645@gmail.com>
2024-10-27 11:17:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import streamlit as st
 st.set_page_config(
-    page_title="Genetic Algorithm hfzh"
+    page_title="Genetic Algorithm"
 )
 
-st.header("Genetic Algorithm", divider="gray")
+st.header("Genetic Algorithm Exercise", divider="gray")
 
 import random
 
