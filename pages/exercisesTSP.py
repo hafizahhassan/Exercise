23b0208153b857209f570239060382fb907abd8b
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-11-05 00:21:59 +0800

    Update exercisesTSP.py

--- pages/exercisesTSP.py
+++ pages/exercisesTSP.py
@@ -47,7 +47,7 @@ if submitButton:
 
     for i, (city, (city_x, city_y)) in enumerate(city_coords.items()):
     color = colors[i]
-    icon = city_icons[i + 1]
+    icon = city_icons[city]
     ax.scatter(city_x, city_y, c=[color], s=1200, zorder=2)
     ax.annotate(icon, (city_x, city_y), fontsize=40, ha='center', va='center', zorder=3)
     ax.annotate(city, (city_x, city_y), fontsize=12, ha='center', va='bottom', xytext=(0, -30),
