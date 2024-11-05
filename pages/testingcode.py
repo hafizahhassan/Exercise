f387801f1281e12d3b5a9ccb3f1cb290c096f546
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-11-05 14:16:53 +0800

    Update testingcode.py

--- pages/testingcode.py
+++ pages/testingcode.py
@@ -1,3 +1,4 @@
+
 import matplotlib.pyplot as plt
 from itertools import permutations, combinations
 from random import shuffle
@@ -8,56 +9,49 @@ import pandas as pd
 import seaborn as sns
 import streamlit as st
 
-st.title("Cities Coordinate Input TSP")
+x = [0,3,6,7,15,10,16,5,8,1.5]
+y = [1,2,1,4.5,-1,2.5,11,6,9,12]
+cities_names = ["Gliwice", "Cairo", "Rome", "Krakow", "Paris", "Alexandria", "Berlin", "Tokyo", "Rio", "Budapest"]
+city_coords = dict(zip(cities_names, zip(x, y)))
+n_population = 250
+crossover_per = 0.8
+mutation_per = 0.2
+n_generations = 200
 
-# Create input form for cities
-with st.form("city_input_form"):
-    city_coords = {}
-    for i in range(1, 11):
-        col1, col2, col3 = st.columns(3)    #Buat 3 column
-        cities_names = col1.text_input(f"City {i}", f"City {i}")
-        x = col2.number_input(f"X Coordinate for {cities_names}", min_value=1, max_value=10, step=1, key=f"x{i}")
-        y = col3.number_input(f"Y Coordinate for {cities_names}", min_value=1, max_value=10, step=1, key=f"y{i}")
-        city_coords[cities_names] = (x, y)
-        
-    # Button
-    submitButton = st.form_submit_button("Submit")
+# Pastel Pallete
+colors = sns.color_palette("pastel", len(cities_names))
 
-# Code untuk button
-if submitButton:
-    cityName = list(city_coords.keys())
+# City Icons
+city_icons = {
+    "Gliwice": "♕",
+    "Cairo": "♖",
+    "Rome": "♗",
+    "Krakow": "♘",
+    "Paris": "♙",
+    "Alexandria": "♔",
+    "Berlin": "♚",
+    "Tokyo": "♛",
+    "Rio": "♜",
+    "Budapest": "♝"
+}
 
-    # Pastel palette
-    colors = sns.color_palette("pastel", 10)
+fig, ax = plt.subplots()
 
-    # Define default settings for the genetic algorithm
-    n_population = 250
-    crossover_per = 0.8
-    mutation_per = 0.2
-    n_generations = 200
+ax.grid(False)  # Grid
 
-    # City Icons
-    city_icons = {
-        1: "♕", 2: "♖", 3: "♗", 4: "♘", 5: "♙",6: "♔", 7: "♚",8: "♛", 9: "♜", 10: "♝"
-    }
+for i, (city, (city_x, city_y)) in enumerate(city_coords.items()):
+    color = colors[i]
+    icon = city_icons[city]
+    ax.scatter(city_x, city_y, c=[color], s=1200, zorder=2)
+    ax.annotate(icon, (city_x, city_y), fontsize=40, ha='center', va='center', zorder=3)
+    ax.annotate(city, (city_x, city_y), fontsize=12, ha='center', va='bottom', xytext=(0, -30),
+                textcoords='offset points')
 
-    # Plotting city
-    fig, ax = plt.subplots()
-    ax.grid(False)  # Grid
+    # Connect cities with opaque lines
+    for j, (other_city, (other_x, other_y)) in enumerate(city_coords.items()):
+        if i != j:
+            ax.plot([city_x, other_x], [city_y, other_y], color='gray', linestyle='-', linewidth=1, alpha=0.1)
 
-    for i, (city, (city_x, city_y)) in enumerate(city_coords.items()):
-        color = colors[i]
-        icon = city_icons[city]
-        ax.scatter(city_x, city_y, c=[color], s=1200, zorder=2)
-        ax.annotate(icon, (city_x, city_y), fontsize=40, ha='center', va='center', zorder=3)
-        ax.annotate(city, (city_x, city_y), fontsize=12, ha='center', va='bottom', xytext=(0, -30),
-                    textcoords='offset points')
-
-        # Connect cities with opaque lines
-        for j, (other_city, (other_x, other_y)) in enumerate(city_coords.items()):
-            if i != j:
-                ax.plot([city_x, other_x], [city_y, other_y], color='gray', linestyle='-', linewidth=1, alpha=0.1)
-
-    fig.set_size_inches(16, 12)
-    #plt.show()
-    st.pyplot(fig)
+fig.set_size_inches(16, 12)
+#plt.show()
+st.pyplot(fig)
