94f518621cb81e88c25c913908458ec2f42849f3
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-11-05 13:27:25 +0800

    Update testingcode.py

--- pages/testingcode.py
+++ pages/testingcode.py
@@ -17,8 +17,8 @@ with st.form("city_input_form"):
     for i in range(1, 11):
         col1, col2, col3 = st.columns(3)    #Buat 3 column
         cities_names = col1.text_input(f"City {i}", f"City {i}")
-        x = col2.number_input(f"X Coordinate for {cities_names}", min_value=1, max_value=10, step=1, key=f"x{i}")
-        y = col3.number_input(f"Y Coordinate for {cities_names}", min_value=1, max_value=10, step=1, key=f"y{i}")
+        x = col2.number_input(f"X Coordinate", min_value=1, max_value=10, step=1, key=f"x{i}")
+        y = col3.number_input(f"Y Coordinate", min_value=1, max_value=10, step=1, key=f"y{i}")
         city_coords[cities_names] = (x, y)
         
     # Button
