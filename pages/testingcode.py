a71f11c3702491a64e1807bbbc945acfedb2e7cd
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-11-05 14:22:45 +0800

    Update testingcode.py

--- pages/testingcode.py
+++ pages/testingcode.py
@@ -9,9 +9,9 @@ import pandas as pd
 import seaborn as sns
 import streamlit as st
 
-cities_names = text_input(f"City {i}", f"City {i}")
-x = number_input(f"X Coordinate for {cities_names}", min_value=1, max_value=10, step=1, key=f"x{i}")
-y = number_input(f"Y Coordinate for {cities_names}", min_value=1, max_value=10, step=1, key=f"y{i}")
+cities_names = st.text_input(f"City {i}", f"City {i}")
+x = st.number_input(f"X Coordinate for {cities_names}", min_value=1, max_value=10, step=1, key=f"x{i}")
+y = st.number_input(f"Y Coordinate for {cities_names}", min_value=1, max_value=10, step=1, key=f"y{i}")
 city_coords = dict(zip(cities_names, zip(x, y)))
 n_population = 250
 crossover_per = 0.8
