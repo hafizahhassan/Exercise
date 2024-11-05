f230b3000d7a627374004ce441ee7981be85073c
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-11-05 14:51:55 +0800

    Update exercisesTSP.py

--- pages/exercisesTSP.py
+++ pages/exercisesTSP.py
@@ -13,7 +13,7 @@ st.title("Cities Coordinate Input TSP")
 # Create input form for cities
 with st.form("city_input_form"):
     city_coords = {}
-    for i in range(10):
+    for i in range(1, 11):
         col1, col2, col3 = st.columns(3)    #Buat 3 column
         cities_names = col1.text_input(f"City {i}", f"City {i}")
         x = col2.number_input(f"X Coordinate for {cities_names}", min_value=1, max_value=10, step=1, key=f"x{i}")
