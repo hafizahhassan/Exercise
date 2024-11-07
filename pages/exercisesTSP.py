426525ca8b742205ad26bde727fd72b8e7b51971
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-11-07 16:48:21 +0800

    Update exercisesTSP.py

--- pages/exercisesTSP.py
+++ pages/exercisesTSP.py
@@ -21,14 +21,14 @@ with st.form("city_input_form"):
     for i in range(10):
         col1, col2, col3 = st.columns(3)    #Buat 3 column
         
-        cities_names = col1.text_input(f"City {i+1}", value=f"City {i+1}")
-        cities_names.append(cities_names)
+        city_name = col1.text_input(f"City {i+1}", value=f"City {i+1}")
+        cities_names.append(city_name)
         
-        x = col2.number_input(f"X Coordinate for City {i+1}", min_value=1, max_value=10, step=1)
-        x.append(x)
+        city_x = col2.number_input(f"X Coordinate for City {i+1}", min_value=1, max_value=10, step=1)
+        x.append(city_x)
         
-        y = col3.number_input(f"Y Coordinate for City {i+1}", min_value=1, max_value=10, step=1)
-        y.append(y)
+        city_y = col3.number_input(f"Y Coordinate for City {i+1}", min_value=1, max_value=10, step=1)
+        y.append(city_y)
         
     # Button
     submitButton = st.form_submit_button("Submit")
