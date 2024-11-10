b2edb4b1639cbfe99b6267c6f92e6f149d0ae095
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-11-10 17:16:20 +0800

    Update ES - mu, lamda.py

--- pages/ES - mu, lamda.py
+++ pages/ES - mu, lamda.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# evolution strategy (mu, lambda) of the ackley objective function
+from numpy import asarray
+from numpy import exp
+from numpy import sqrt
+from numpy import cos
+from numpy import e
+from numpy import pi
+from numpy import argsort
+from numpy.random import randn
+from numpy.random import rand
+from numpy.random import seed
+
+st.title("Ackley Multimodal Function")
