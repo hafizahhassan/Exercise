3c3404e0d633b22e63375ecd1d98c71cba7abac4
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-11-10 17:37:06 +0800

    Update ES exercise.py

--- pages/ES exercise.py
+++ pages/ES exercise.py
@@ -1,1 +1,20 @@
+import streamlit as st
 
+import numpy as np
+import matplotlib.pyplot as plt
+#from numpy import arange, exp, sqrt, cos, e, pi, meshgrid
+from mpl_toolkits.mplot3d import Axes3D
+
+# evolution strategy (mu + lambda) of the ackley objective function
+from numpy import asarray
+from numpy import exp
+from numpy import sqrt
+from numpy import cos
+from numpy import e
+from numpy import pi
+from numpy import argsort
+from numpy.random import randn
+from numpy.random import rand
+from numpy.random import seed
+
+st.title("ES Exercise")
