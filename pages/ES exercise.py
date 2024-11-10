aa917ff678d2e03b77eca2d0d5a21fb77f217812
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-11-10 18:11:05 +0800

    Update ES exercise.py

--- pages/ES exercise.py
+++ pages/ES exercise.py
@@ -14,10 +14,11 @@ st.title("Exercise ES")
 with st.form("es_form"):
     st.write("Choose button you want to see the Output : ")
 
-    TARGET = st.text_input("Enter your name : ")
+    col1, col2 = st.columns(2)
+    TARGET = col1.st.text_input("Enter your name : ")
+    submitButton2 = col2st.form_submit_button("Submit")
         
     # Button
     submitButton = st.form_submit_button("Submit")
-    submitButton2 = st.form_submit_button("Submit")
     
 st.write("O U T P U T")
