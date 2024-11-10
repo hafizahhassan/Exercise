947ff096b89b91d22ffd2dbffd796cc1bb1dfee9
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-11-10 23:08:17 +0800

    Update try.py

--- pages/try.py
+++ pages/try.py
@@ -159,59 +159,59 @@ if st.button("Button"):
       offspring_list.append(offspring_1)
       offspring_list.append(offspring_2)
   
-  mixed_offspring = parents_list + offspring_list
+    mixed_offspring = parents_list + offspring_list
   
-  fitness_probs = fitness_prob(mixed_offspring)
-  sorted_fitness_indices = np.argsort(fitness_probs)[::-1]
-  best_fitness_indices = sorted_fitness_indices[0:n_population]
-  best_mixed_offspring = []
-  for i in best_fitness_indices:
-    best_mixed_offspring.append(mixed_offspring[i]) 
+    fitness_probs = fitness_prob(mixed_offspring)
+    sorted_fitness_indices = np.argsort(fitness_probs)[::-1]
+    best_fitness_indices = sorted_fitness_indices[0:n_population]
+    best_mixed_offspring = []
+    for i in best_fitness_indices:
+      best_mixed_offspring.append(mixed_offspring[i]) 
   
-  for i in range(0, n_generations):
+    for i in range(0, n_generations):
   
-    fitness_probs = fitness_prob(best_mixed_offspring)
-    parents_list = []
-    for i in range(0, int(crossover_per * n_population)):
-      parents_list.append(roulette_wheel(best_mixed_offspring, fitness_probs))
+      fitness_probs = fitness_prob(best_mixed_offspring)
+      parents_list = []
+      for i in range(0, int(crossover_per * n_population)):
+        parents_list.append(roulette_wheel(best_mixed_offspring, fitness_probs))
   
-    offspring_list = []
-    for i in range(0,len(parents_list), 2):
-      offspring_1, offspring_2 = crossover(parents_list[i], parents_list[i+1])
+      offspring_list = []
+      for i in range(0,len(parents_list), 2):
+        offspring_1, offspring_2 = crossover(parents_list[i], parents_list[i+1])
   
-      mutate_threashold = random.random()
-      if(mutate_threashold > (1-mutation_per)):
+        mutate_threashold = random.random()
+        if(mutate_threashold > (1-mutation_per)):
           offspring_1 = mutation(offspring_1)
   
-      mutate_threashold = random.random()
-      if(mutate_threashold > (1-mutation_per)):
-          offspring_2 = mutation(offspring_2)
+        mutate_threashold = random.random()
+        if(mutate_threashold > (1-mutation_per)):
+            offspring_2 = mutation(offspring_2)
   
-      offspring_list.append(offspring_1)
-      offspring_list.append(offspring_2)
+        offspring_list.append(offspring_1)
+        offspring_list.append(offspring_2)
   
-  mixed_offspring = parents_list + offspring_list
-  fitness_probs = fitness_prob(mixed_offspring)
-  sorted_fitness_indices = np.argsort(fitness_probs)[::-1]
-  best_fitness_indices = sorted_fitness_indices[0:int(0.8*n_population)]
+      mixed_offspring = parents_list + offspring_list
+      fitness_probs = fitness_prob(mixed_offspring)
+      sorted_fitness_indices = np.argsort(fitness_probs)[::-1]
+      best_fitness_indices = sorted_fitness_indices[0:int(0.8*n_population)]
   
-  best_mixed_offspring = []
-  for i in best_fitness_indices:
-      best_mixed_offspring.append(mixed_offspring[i])
+      best_mixed_offspring = []
+      for i in best_fitness_indices:
+        best_mixed_offspring.append(mixed_offspring[i])
   
-  old_population_indices = [random.randint(0, (n_population - 1)) for j in range(int(0.2*n_population))]
-  for i in old_population_indices:
-      best_mixed_offspring.append(population[i])
+      old_population_indices = [random.randint(0, (n_population - 1)) for j in range(int(0.2*n_population))]
+      for i in old_population_indices:
+        best_mixed_offspring.append(population[i])
   
-  random.shuffle(best_mixed_offspring)
+      random.shuffle(best_mixed_offspring)
   
-  return best_mixed_offspring
+    return best_mixed_offspring
   
   best_mixed_offspring = run_ga(cities_names, n_population, n_generations, crossover_per, mutation_per)
   
   total_dist_all_individuals = []
   for i in range(0, n_population):
-  total_dist_all_individuals.append(total_dist_individual(best_mixed_offspring[i]))
+    total_dist_all_individuals.append(total_dist_individual(best_mixed_offspring[i]))
   
   index_minimum = np.argmin(total_dist_all_individuals)
   
@@ -226,9 +226,9 @@ if st.button("Button"):
   x_shortest = []
   y_shortest = []
   for city in shortest_path:
-  x_value, y_value = city_coords[city]
-  x_shortest.append(x_value)
-  y_shortest.append(y_value)
+    x_value, y_value = city_coords[city]
+    x_shortest.append(x_value)
+    y_shortest.append(y_value)
   
   x_shortest.append(x_shortest[0])
   y_shortest.append(y_shortest[0])
@@ -238,20 +238,20 @@ if st.button("Button"):
   plt.legend()
   
   for i in range(len(x)):
-  for j in range(i + 1, len(x)):
-  ax.plot([x[i], x[j]], [y[i], y[j]], 'k-', alpha=0.09, linewidth=1)
+    for j in range(i + 1, len(x)):
+      ax.plot([x[i], x[j]], [y[i], y[j]], 'k-', alpha=0.09, linewidth=1)
   
   plt.title(label="TSP Best Route Using GA",
-    fontsize=25,
-    color="k")
+            fontsize=25,
+            color="k")
   
   str_params = '\n'+str(n_generations)+' Generations\n'+str(n_population)+' Population Size\n'+str(crossover_per)+' Crossover\n'+str(mutation_per)+' Mutation'
   plt.suptitle("Total Distance Travelled: "+
-       str(round(minimum_distance, 3)) +
-       str_params, fontsize=18, y = 1.047)
+               str(round(minimum_distance, 3)) +
+               str_params, fontsize=18, y = 1.047)
   
   for i, txt in enumerate(shortest_path):
-  ax.annotate(str(i+1)+ "- " + txt, (x_shortest[i], y_shortest[i]), fontsize= 20)
+    ax.annotate(str(i+1)+ "- " + txt, (x_shortest[i], y_shortest[i]), fontsize= 20)
   
   fig.set_size_inches(16, 12)
   st.pyplot(fig)
