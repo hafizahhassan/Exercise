bd59cd7e10198a586ef9f2a27efc8d2f42e95767
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-11-10 17:15:33 +0800

    Update ES - Ackley.py

--- pages/ES - Ackley.py
+++ pages/ES - Ackley.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy import arange, exp, sqrt, cos, e, pi, meshgrid
+#from numpy import arange, exp, sqrt, cos, e, pi, meshgrid
 from mpl_toolkits.mplot3d import Axes3D
 
 # ackley multimodal function
