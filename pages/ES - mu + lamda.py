a3c90c2994aca122e51bf4908b4d05693a81ef69
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-11-10 17:18:08 +0800

    Update ES - mu + lamda.py

--- pages/ES - mu + lamda.py
+++ pages/ES - mu + lamda.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+import numpy as np
+import matplotlib.pyplot as plt
+#from numpy import arange, exp, sqrt, cos, e, pi, meshgrid
+from mpl_toolkits.mplot3d import Axes3D
+
+st.title("Develop a 'mu + lamda' in ES")
