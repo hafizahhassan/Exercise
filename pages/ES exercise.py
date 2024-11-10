f1671e38a33adb849a9aba8b635e55387c0dd160
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-11-10 18:24:16 +0800

    Update ES exercise.py

--- pages/ES exercise.py
+++ pages/ES exercise.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Title for the app
 st.title("Form with Two Buttons")
 
-col1, col3 = st.columns(2)
+col1, col2 = st.columns(2)
 submit_button = col1.form_submit_button("Submit")
 clear_button = col2.button("Clear Form")
 
