033171d4406fb3892af3b7a2c906c85b8e9b0f46
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-12-03 16:47:25 +0800

    Update Assignment EC.py

--- pages/Assignment EC.py
+++ pages/Assignment EC.py
@@ -24,7 +24,7 @@ def read_csv_to_dict(file_path):
       return program_ratings
 
     except requests.exceptions.RequestException as e:
-      print(f"Error fetching or processing CSV data: {e}")
+      st.write(f"Error fetching or processing CSV data: {e}")
       return None  # or raise the exception if you prefer
 
 # Path to the CSV file
@@ -35,7 +35,7 @@ program_ratings_dict = read_csv_to_dict(file_path)
 
 # Print the result (you can also return or process it further)
 for program, ratings in program_ratings_dict.items():
-    print(f"'{program}': {ratings},")
+    st.write(f"'{program}': {ratings},")
 
 
 import random
@@ -158,8 +158,11 @@ genetic_schedule = genetic_algorithm(initial_best_schedule, generations=GEN, pop
 
 final_schedule = initial_best_schedule + genetic_schedule[:rem_t_slots]
 
-print("\nFinal Optimal Schedule:")
+st.write("\nFinal Optimal Schedule:")
 for time_slot, program in enumerate(final_schedule):
     print(f"Time Slot {all_time_slots[time_slot]:02d}:00 - Program {program}")
 
-print("Total Ratings:", fitness_function(final_schedule))
+st.write("Total Ratings:", fitness_function(final_schedule))
+
+fig.set_size_inches(16, 12)  
+st.pyplot(fig)
