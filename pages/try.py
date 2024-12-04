d1673dcd35ff7a64f26387a68786e6a8908da53f
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-12-04 13:56:26 +0800

    Update try.py

--- pages/try.py
+++ pages/try.py
@@ -33,7 +33,7 @@ def read_csv_to_dict(file_path):
 file_path = 'https://raw.githubusercontent.com/hafizahhassan/Exercise/refs/heads/main/pages/program_ratings.csv'
 
 # Get the data in the required format
-program_ratings_dict = read_csv_to_dict(file_path)
+program_ratings_dict = read_csv_to_dataframe(file_path)
 
 # Print the result (you can also return or process it further)
 for program, ratings in program_ratings_dict.items():
