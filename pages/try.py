93103068d7c7d5960aa5247639a256ec1b392471
Nur Hafizah Hassan <hfzhhssn645@gmail.com>
2024-12-04 17:05:27 +0800

    Update try.py

--- pages/try.py
+++ pages/try.py
@@ -63,9 +63,10 @@ st.subheader("This is a subheader with a colored divider", divider="green")
 st.markdown("""
     <style>
     hr {
-        #border-color: darkblue;
-        border-top: 5px solid red;
-        width: 50%;
+        border-color: darkblue;
+        height: 4px;
+        #border-top: 5px solid red;
+        #width: 50%;
     }
     </style>
 """, unsafe_allow_html=True)
